a9ae595f5ff4fd20cd16f3d0c458c26b793b08b8
pedro-dominici <pedro_dominici@outlook.com>
2022-04-28 18:04:15 -0300

    fix: remove stories

--- src/components/CardBlock/stories.tsx
+++
@@ -1,9 +0,0 @@
-// import { Story, Meta } from '@storybook/react'
-// import CardBlock from '.'
-
-// export default {
-//   title: 'CardBlock',
-//   component: CardBlock
-// } as Meta
-
-// export const Default: Story = () => <CardBlock />
