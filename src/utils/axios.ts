3913126d361f54df47240df61ec3cfbec2c42026
pedro-dominici <pedro_dominici@outlook.com>
2022-05-30 13:06:16 -0300

    fix: api url

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { setupCache } from 'axios-cache-adapter'
 
-const baseURL = 'http://localhost:3000/api'
+const baseURL = process.env.API_URL
 
 const cache = setupCache({
   maxAge: 60 * 1000
