33a39b063280f0415d94e33bdcc8f9547cf1d668
Arthur Santos Nardini <arthurnardini@ext.cvccorp.com.br>
2022-06-23 19:39:04 -0300

    feat: abuble

---
+++ src/components/PDF/index.tsx
@@ -0,0 +1,33 @@
+import { format } from 'date-fns'
+import Barcode from 'react-barcode'
+import QRCode from 'react-qr-code'
+import { Logo } from '../Icons'
+import * as S from './styles'
+
+interface IPDFProps {
+  id: string
+}
+
+function PDF({ id }: IPDFProps) {
+  const date = format(new Date(), "dd/MM/yyyy '-' HH'h'mm ")
+  return (
+    <S.Container>
+      <S.TitleContainer>
+        <Logo option={'header'} />
+        <S.Title>IZI Vagas</S.Title>
+      </S.TitleContainer>
+      <S.Line />
+      <S.ContentContainer>
+        <h1>Ticket de Estacionamento</h1>
+        <h2>{date}</h2>
+        <S.Line />
+        <QRCode value={id} />
+        <p>{id}</p>
+        <S.Line />
+        <Barcode value={id} />
+      </S.ContentContainer>
+    </S.Container>
+  )
+}
+
+export default PDF
