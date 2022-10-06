bd39c61564ca7bce365a4e5b5bd88768beecf71f
Arthur Santos Nardini <arthurnardini@ext.cvccorp.com.br>
2022-10-05 22:07:30 -0300

    feat: added visual feedbacks to clients page

--- src/pages/blocks/index.tsx
+++ src/pages/blocks/index.tsx
@@ -1,12 +1,16 @@
-import { useEffect, useState } from 'react'
-import * as S from './styles'
+import { Blocks } from '.prisma/client'
 import CardBlock from '@/components/CardBlock'
 import Header from '@/components/Header'
 import Indicator from '@/components/Indicator'
-import clientApi from '@/utils/axios'
-import { Blocks } from '.prisma/client'
-import getColor from '@/utils/getColor'
 import { BoxSkeleton } from '@/styles/skeleton'
+import clientApi from '@/utils/axios'
+import getColor from '@/utils/getColor'
+import { useRouter, withRouter } from 'next/router'
+import { useEffect, useState } from 'react'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import * as S from './styles'
+
 interface IBlocksOccupied extends Blocks {
   occupied: number
 }
@@ -18,7 +22,8 @@ type StateBlocks = {
   errorMessage: string
 }
 
-export default function BlocksPage() {
+function BlocksPage() {
+  const router = useRouter()
   const [state, setState] = useState<StateBlocks>({
     blocks: [],
     error: false,
@@ -42,8 +47,34 @@ export default function BlocksPage() {
         }))
       )
   }, [])
+
+  useEffect(() => {
+    if (router.query.previousPage === 'index') {
+      toast.success('Ticket gerado com sucesso!', {
+        position: 'top-right',
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined
+      })
+    }
+  }, [])
+
   return (
     <S.Container>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
       <Header title="Blocos" />
       <Indicator ClassName="indicator" />
       {state.error && <p>{state.errorMessage}</p>}
@@ -68,3 +99,5 @@ export default function BlocksPage() {
     </S.Container>
   )
 }
+
+export default withRouter(BlocksPage)
