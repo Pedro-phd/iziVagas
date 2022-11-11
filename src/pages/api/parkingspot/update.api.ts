f9c07086fd1eab0b8ee3471fdb81edc8f1afc41d
pedro-phd <pedro_dominici@outlook.com>
2022-11-11 13:15:32 -0300

    fix: change update api

--- src/pages/api/parkingspot/update.api.ts
+++ src/pages/api/parkingspot/update.api.ts
@@ -1,13 +1,24 @@
 import prisma from '@/../prisma/client'
-import { ParkingSpot } from '@prisma/client'
 import { NextApiRequest, NextApiResponse } from 'next'
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
+  type ParkingSpot = {
+    id?: string
+    name: string
+    block: string
+    blockID: string
+    occupied: boolean
+    special: boolean
+    old: boolean
+  }
+
+  const { id }: ParkingSpot = req.body
   const data: ParkingSpot = req.body
+  delete data.id
   prisma.parkingSpot
     .update({
       where: {
-        id: data.id
+        id: id
       },
       data: {
         ...data
